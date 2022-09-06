19416f5ed7d02a8c3f75967446cddc07fe648a94
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-07 02:29:01 +0530

    Update k8s & cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,6 @@
 gcloud auth application-default login
 cd terraform
 terraform init && terraform apply
+gcloud container clusters get-credentials primary --zone us-central1-a --project gpt-j-and-gpt-neox20b
+kubectl get svc
+kubectl get nodes
