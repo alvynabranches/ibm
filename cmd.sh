7c0c875f9c8834a8771e19695b4f3ae3df816a42
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-07 15:41:29 +0530

    Added additional commands

--- cmd.sh
+++ cmd.sh
@@ -5,3 +5,9 @@ gcloud container clusters get-credentials primary --zone us-central1-a --project
 kubectl get svc
 kubectl get nodes
 kubectl get pods
+kubectl apply -f k8s/prod/app-deployment.yaml
+kubectl apply -f k8s/prod/cloud-deployment.yaml
+kubectl get ns
+kubectl get pods -n prod
+kubectl exec -n -it <name>
+# staging alpha storage ls
